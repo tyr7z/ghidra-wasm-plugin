e56855102ec71bf961ca6f3bdd6db11dfbf24e29
Robert Xiao <brx@cs.ubc.ca>
2021-09-04 04:08:05 -0600

    Initialize all non-parameter locals to zero

--- src/main/java/wasm/pcode/InjectPayloadWasmEntry.java
+++ src/main/java/wasm/pcode/InjectPayloadWasmEntry.java
@@ -33,8 +33,13 @@ public class InjectPayloadWasmEntry extends InjectPayloadSleigh {
 		Address inputBase = program.getRegister("i0").getAddress();
 		Address localsBase = program.getRegister("l0").getAddress();
 		ValType[] params = funcAnalysis.getSignature().getParams();
+		ValType[] locals = funcAnalysis.getSignature().getLocals();
 		for (int i = 0; i < params.length; i++) {
 			ops.emitCopy(inputBase.add(i * 8L), localsBase.add(i * 8L), params[i].getSize());
+		}
+		Address zero = program.getAddressFactory().getConstantAddress(0L);
+		for (int i = params.length; i < locals.length; i++) {
+			ops.emitCopy(zero, localsBase.add(i * 8L), locals[i].getSize());
 		}
 		return ops.getPcodeOps();
 	}
